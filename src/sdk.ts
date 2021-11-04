dd6351223b8cb35cc9235d8584fb9003eedbaaef
jiangplus <jiang.plus.times@gmail.com>
2021-11-05 01:54:57 +0800

    添加abi和合约地址列表

--- src/sdk.ts
+++ src/sdk.ts
@@ -2,6 +2,71 @@ import { ethers, Signer, BigNumber } from "ethers";
 import { keccak_256 } from 'js-sha3';
 import { Buffer as Buffer } from "buffer/";
 
+import { Provider as AbstractWeb3Provider } from "@ethersproject/abstract-provider"
+import { Signer as Web3Signer } from "@ethersproject/abstract-signer"
+
+import { ResolverAbi, ControllerAbi, PnsAbi } from "./abi";
+import { Chains, IContractAddrs, IContractAddrsMap, ContractAddrMap } from "./constants";
+
+
+export type HexAddress = string;
+
+export type DomainString = string;
+
+export interface ContentType {
+  value: string;
+  contentType: string;
+}
+
+declare abstract class Web3Provider extends AbstractWeb3Provider {
+  abstract getSigner(): Promise<Web3Signer>;
+}
+
+export type DomainDetails = {
+  name: string;
+  label: string;
+  labelhash: string;
+  owner: string;
+  nameResolver: string;
+
+  content: string;
+  contentType?: string;
+
+  addrs: {
+    key: string;
+    value: string;
+  }[];
+  textRecords: {
+    key: string;
+    value: string;
+  }[];
+};
+
+export const formatEther = ethers.utils.formatEther
+
+const TEXT_RECORD_KEYS = ["email", "url", "avatar", "description", "notice", "keywords", "com.twitter", "com.github"];
+
+const emptyAddress = "0x0000000000000000000000000000000000000000";
+const emptyNode = "0x0000000000000000000000000000000000000000000000000000000000000000";
+
+const tld = "dot";
+const DAYS = 24 * 60 * 60;
+const INFURA_URL = "https://rinkeby.infura.io/v3/75e0d27975114086be0463cf2597549e";
+
+
+let provider: Web3Provider;
+let signer: Web3Signer;
+let account: string;
+let networkId: number;
+
+let pns: any;
+let resolver: any;
+let registrar: any;
+
+let pnsAddr: string;
+let resolverAddr: string;
+let registrarAddr: string;
+
 export function sha3 (data: string) {
   return "0x" + keccak_256(data)
 }
@@ -20,3 +85,7 @@ export function getNamehash (name: string) {
 
   return '0x' + node
 }
+
+
+
+
